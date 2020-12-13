9a568cfd103c7504467a6f2113607027b633520a
ashikkhandani <mdashikahammed@gmail.com>
2020-12-13 21:46:56 +0100

    css and js modified

--- script.js
+++ script.js
@@ -161,7 +161,7 @@ document.querySelector('.check').addEventListener('click',function(){
         document.querySelector('body').style.backgroundColor = "#60b347";
 
         // change number box size
-        document.querySelector('.number').style.width = "30rem";
+        document.querySelector('.number').style.width = "25rem";
 
         // show high score
         if(score > highScore){
